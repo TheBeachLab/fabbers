544b70bdaa9689cfdc79bf55f16bcc700ba8d802
Fran <hola@beachlab.org>
2019-12-22 07:56:32 +0100

    profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(debugShowCheckedModeBanner: false),
-  );
+  runApp(FabberApp());
+}
+
+class FabberApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/character.png'),
+              ),
+              Text(
+                'fabulous fabber',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
