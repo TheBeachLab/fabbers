1c840960ae6dd38529700ce540dc648bb67671b6
Fran <hola@beachlab.org>
2019-12-23 18:23:14 +0100

    cards with rows

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,58 @@ class FabberApp extends StatelessWidget {
                 'find amazing people with incredible skills',
                 style: TextStyle(
                   fontFamily: 'PatuaOne',
-                  color: Colors.grey.shade300,
+                  color: Colors.deepOrange.shade200,
                   fontSize: 20.0,
                 ),
-              )
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.explore,
+                        size: 45.0,
+                      ),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text(
+                        'http://www.fabbers.io',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        size: 45.0,
+                      ),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text(
+                        'hola@beachlab.org',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
